761d10cbbbf285c163e3dd54c251e2f297676d73
Kayusme <emmanuel4kchrist@gmail.com>
2023-08-24 16:10:30 +0100

    veera

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+// Tells the Solidity compiler to compile only from v0.8.13 to v0.9.0
+pragma solidity ^0.8.13;
 
 contract Auth {
     struct User {
@@ -10,10 +12,7 @@ contract Auth {
     mapping(address => string[]) public loginAttempts;
     mapping(address => User) public users;
     bool public systemActive = true;
-    }
-
-    mapping(address => User) public users;
-
+    
     function register(address _address, string memory _device, string memory _ip) public {
         User memory newUser = User(_address, _device, _ip);
         users[_address] = newUser;
@@ -32,7 +31,7 @@ contract Auth {
             loginAttempts[_address].push("Failed login attempt");
             if (failedLoginAttempts >= 2) {
                 systemActive = false;
-                lastFailedLoginAttempt = now + 2 minutes;
+                lastFailedLoginAttempt = block.timestamp + 2 minutes;
             }
             return false;
         }
@@ -45,4 +44,3 @@ contract Auth {
         delete users[_address];
     }
 }
-
--- src/main.js
+++ src/main.js
@@ -1,34 +1,55 @@
-import Web3 from 'web3';
+//import AuthABI from "./contracts/Auth.json";
 
-let web3 = new Web3('http://localhost:8545');
-let authContract = new web3.eth.Contract(AuthABI, AuthAddress);
+fetch('./contracts/Auth.json')
+  .then(response => response.json())
+  .then(async AuthABI => {
+    try {
+      let web3 = new Web3(new Web3.providers.HttpProvider("https://data-seed-prebsc-1-s1.binance.org:8545"));
+      let authContract = new web3.eth.Contract(AuthABI.abi, '0xefa5c820bCb6139Eb93c22111C7866FaF784B0c8');
+      console.log(AuthABI.abi);
+      console.log("auth file: ", AuthABI);
+      const networkId = await web3.eth.net.getId();
+      console.log("network id: ", networkId);
+      const deployedNetwork = AuthABI.networks[networkId];
+      
+      if (!deployedNetwork) {
+        throw new Error('Contract not deployed on the current network');
+      }
 
-function register() {
-    let userAddress = document.getElementById('userAddress').value;
-    let device = document.getElementById('device').value;
-    let ip = document.getElementById('ip').value;
-    authContract.methods.register(userAddress, device, ip).send({from: userAddress});
-}
+      console.log("deployedNetwork id: ", deployedNetwork);
+      
 
-function login() {
-    let userAddress = document.getElementById('userAddress').value;
-    let ip = document.getElementById('ip').value;
-    authContract.methods.login(userAddress, ip).call({from: userAddress});
-}
+      async function register() {
+        let userAddress = document.getElementById('userAddress').value;
+        let device = document.getElementById('device').value;
+        let ip = document.getElementById('ip').value;
+        await authContract.methods.register(userAddress, device, ip).send({ from: userAddress });
+      }
 
-function logout() {
-    let userAddress = document.getElementById('userAddress').value;
-    authContract.methods.logout(userAddress).send({from: userAddress});
-}
+      async function login() {
+        let userAddress = document.getElementById('userAddress').value;
+        let ip = document.getElementById('ip').value;
+        await authContract.methods.login(userAddress, ip).call({from: userAddress});
+      }
 
-function displayLogs() {
-    let userAddress = document.getElementById('userAddress').value;
-    let logs = authContract.methods.loginAttempts(userAddress).call({from: userAddress});
-    console.log(logs);
-}
+      async function logout() {
+        let userAddress = document.getElementById('userAddress').value;
+        await authContract.methods.logout(userAddress).send({from: userAddress});
+      }
 
-document.getElementById('registerButton').addEventListener('click', register);
-document.getElementById('loginButton').addEventListener('click', login);
-document.getElementById('logoutButton').addEventListener('click', logout);
-document.getElementById('displayLogsButton').addEventListener('click', displayLogs);
+      async function displayLogs() {
+        let userAddress = document.getElementById('userAddress').value;
+        let logs = await authContract.methods.loginAttempts(userAddress).call({from: userAddress});
+        console.log(logs);
+      }
 
+      document.getElementById('registerButton').addEventListener('click', register);
+      document.getElementById('loginButton').addEventListener('click', login);
+      document.getElementById('logoutButton').addEventListener('click', logout);
+      document.getElementById('displayLogsButton').addEventListener('click', displayLogs);
+
+    } catch (error) {
+      console.error('Error initializing contract:', error);
+    }
+  })
+  .catch(error => console.error('Error loading Auth.json:', error));
